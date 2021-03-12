4a5da1277aefa468b2623010f2de7fd22c8c3075
alexandro <alexandroyedra@gmail.com>
2021-03-12 11:17:09 -0700

    added css class to Movie component

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './Movie.css'
 
 const apiKey = '1a09dcf42c6c621e5b547c2f53c489b3'
 
 export default function Movie({title, poster, usersScore}) {
     const posterURL = `https://image.tmdb.org/t/p/w500/${poster}?api_key=${apiKey}`
     return (
-        <div>
+        <div className="Movie">
             <p>{title}</p>
             <img src={posterURL} alt="movie_poster"/>
-            {usersScore}
+            <p>{usersScore}</p>
         </div>
     )
 }
