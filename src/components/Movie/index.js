9e6d5e79d1567fad1bc1418dc0fbe6f63d02e4f4
alexandro <alexandroyedra@gmail.com>
2021-03-11 19:25:35 -0700

    added url const

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+const apiKey = '1a09dcf42c6c621e5b547c2f53c489b3'
+
 export default function Movie({title, poster, usersScore}) {
+    const posterURL = `https://image.tmdb.org/t/p/w500/${poster}?api_key=${apiKey}`
     return (
         <div>
             <p>{title}</p>
-            <img src={poster} alt="movie_poster"/>
+            <img src={posterURL} alt="movie_poster"/>
             {usersScore}
         </div>
     )
