333d2cb732ea9d2bfda9350860bcc34160f0a6c6
alexandro <alexandroyedra@gmail.com>
2021-03-14 09:38:49 -0700

    basic detail movie component

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import useMovie from 'hooks/useMovie'
+
+const apiKey = '1a09dcf42c6c621e5b547c2f53c489b3'
 
 export default function Detail() {
     let { showId } = useParams()
-    return <div>Now showing post {showId}</div>
+    const {movie, isLoading} = useMovie({id: showId})
+
+    if (isLoading) {
+        return <div>cargando...</div>
+    } 
+    
+    return (
+        <div className="Movie">
+            <p>{movie.original_title}</p>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}?api_key=${apiKey}`} alt="movie_poster"/>
+            <p>{movie.overview}</p>
+            <p>{movie.popularity}</p>
+        </div>
+    )
+
 }
