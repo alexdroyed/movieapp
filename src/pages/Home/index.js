56f004b75ba7dc560d3862b768de6f449bd61af0
alexandro <alexandroyedra@gmail.com>
2021-03-14 09:41:23 -0700

    added categorie state to home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ListOfMovies from 'components/ListOfMovies'
+import Button from 'components/Button'
+
+import './Home.css'
 
 export default function Home() {
+    const [categorie, setCategorie] = useState()
+    
+    const handlePopularChange = () => setCategorie("popular")
+
+    const handleTopRatedChange = () => setCategorie("top_rated")
+
+    const handleNowPalyingChange = () => setCategorie("now_playing")
+
     return (
-        <ListOfMovies />
+        <div className="Home">
+            <div className="Categories">
+                <Button handleClick={handlePopularChange} text="populares"/>
+                <Button handleClick={handleTopRatedChange} text="mejores valorados"/>
+                <Button handleClick={handleNowPalyingChange} text="mas vistos"/>
+            </div>
+                
+            <ListOfMovies categorie={categorie}/>
+        </div>
     )
 }
