60c674547d1bf572d3063737d9e7364d704ad758
alexandro <alexandroyedra@gmail.com>
2021-03-14 13:20:21 -0700

    refactoring movie, added link to detail comp

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './Movie.css'
 
 const apiKey = '1a09dcf42c6c621e5b547c2f53c489b3'
 
-export default function Movie({title, poster, usersScore}) {
+export default function Movie({id, title, poster, usersScore}) {
     const posterURL = `https://image.tmdb.org/t/p/w500/${poster}?api_key=${apiKey}`
     return (
-        <div className="Movie">
-            <p>{title}</p>
-            <img src={posterURL} alt="movie_poster"/>
-            <p>{usersScore}</p>
-        </div>
+        <Link to={`show/${id}`} style={{textDecoration: 'none', color: '#555555'}}> 
+            <div className="Movie">
+                <img src={posterURL} alt="movie_poster"/>
+                <h2 className="Movie-title">{title}</h2>
+                <p className="Movie-score">{usersScore}⭐</p>
+            </div>
+        </Link>
     )
 }
