23be484d5a58e569c8140cea7d482081414d5f21
alexandro <alexandroyedra@gmail.com>
2021-03-14 20:37:38 -0700

    Refactor Detail component

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import MovieDetail from 'components/MovieDetail'
 import useMovie from 'hooks/useMovie'
 
-const apiKey = '1a09dcf42c6c621e5b547c2f53c489b3'
+import './Detail.css'
 
 export default function Detail() {
     let { showId } = useParams()
@@ -10,15 +11,14 @@ export default function Detail() {
 
     if (isLoading) {
         return <div>cargando...</div>
-    } 
+    }
     
     return (
-        <div className="Movie">
-            <p>{movie.original_title}</p>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}?api_key=${apiKey}`} alt="movie_poster"/>
-            <p>{movie.overview}</p>
-            <p>{movie.popularity}</p>
+        <div className="Detail">
+            <Link to="/">Home</Link>
+            <MovieDetail movie={movie} />   
         </div>
+
     )
 
 }
