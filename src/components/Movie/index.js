28a5ae2ec66afdd16d68a707911735383e634877
alexandro <alexandroyedra@gmail.com>
2021-03-15 14:38:08 -0700

    refactoring Movie to implemente fav actions

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -4,15 +4,23 @@ import './Movie.css'
 
 const apiKey = '1a09dcf42c6c621e5b547c2f53c489b3'
 
-export default function Movie({id, title, poster, usersScore}) {
+export default function Movie({id, title, poster, usersScore, handleFav}) {
     const posterURL = `https://image.tmdb.org/t/p/w500/${poster}?api_key=${apiKey}`
+
+    const handleFavChange = () => {
+        // handle fav
+    }
+        
     return (
-        <Link to={`show/${id}`} style={{textDecoration: 'none', color: '#555555'}}> 
-            <div className="Movie">
+        <div className="Movie">
+            <Link to={`show/${id}`} style={{textDecoration: 'none', color: '#555555'}}> 
                 <img src={posterURL} alt="movie_poster"/>
-                <h2 className="Movie-title">{title}</h2>
-                <p className="Movie-score">{usersScore}⭐</p>
+                <h2 className="MovieTitle">{title}</h2>
+            </Link>
+            <div className="MovieFooter">
+                <p className="MovieScore">{usersScore}⭐</p>
+                <button onClick={handleFavChange}>fav🤍</button>
             </div>
-        </Link>
+        </div>
     )
 }
