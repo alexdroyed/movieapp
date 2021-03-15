2e9c4133d94cc2a3eefc3123db02095de724854a
alexandro <alexandroyedra@gmail.com>
2021-03-15 14:39:30 -0700

    added remaining fields

--- src/components/MovieDetail/index.js
+++ src/components/MovieDetail/index.js
@@ -11,14 +11,14 @@ export default function MovieDetail({movie}) {
         <div className="MovieDetail">
             <img src={`https://image.tmdb.org/t/p/w500${poster_path}?api_key=${apiKey}`} alt="movie_poster"/>
             <h2 className="Title">{title}</h2>
-            <p className="Overview">{overview}</p>
-            <p className="VoteAverage">{vote_average}</p>
-            <p className="Runtime">{runtime}</p>
             <ul className="Genres">
                 {genres.map(({id, name}) => {
                     return <li key={id}>{name}</li> 
                 })}
             </ul>
+            <p className="Overview">{overview}</p>
+            <p className="VoteAverage">{vote_average}⭐</p>
+            <p className="Runtime">{runtime}</p>
         </div>
     )
 }
