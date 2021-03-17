1f9b363cff7ddbc829ec5bf55563996cb70c80bf
alexandro <alexandroyedra@gmail.com>
2021-03-17 00:14:26 -0700

    just a comment description

--- src/hooks/useMovie.js
+++ src/hooks/useMovie.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react'
 import getMovie from 'services/getMovie'
 
+/** obtains a movie by id selected */
 export default function useMovie({ id }) {
     const [movie, setMovie] = useState()
     const [isLoading, setIsLoading] = useState(true)
