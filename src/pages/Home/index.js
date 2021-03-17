8649ed38de1bd210de2702a6e9e7c59eb7b9b303
alexandro <alexandroyedra@gmail.com>
2021-03-17 00:17:39 -0700

    refactoring the Home component by extracting all logic to hooks and components to have alened and performanced component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,64 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import ListOfMovies from 'components/ListOfMovies'
-import Button from 'components/Button'
-import getMovies from 'services/getMovies'
+import MovieCategories from 'components/MovieCategories'
+import SelectSort from 'components/SelectSort'
+import ListOfMoviesLoader from 'components/ListOfMoviesLoader'
+import useMovies from 'hooks/useMovies'
+import useSort from 'hooks/useSort'
 
 import './Home.css'
 
 export default function Home() {
     const [categorie, setCategorie] = useState()
-    const [movies, setMovies] = useState([])
-    const [sortedMovies, setSortedMovies] = useState([])
-    const [selectedSorted, setSelectedSorted] = useState('default')
-
-    useEffect(() => {
-        getMovies({categorie}).then(movies => setMovies(movies))
-    }, [categorie])
-
-    useEffect(() => {
-        if (selectedSorted === "sortAlphabetically") {
-            const sorted = [...movies].sort(function(a, b){
-                if(a.title < b.title) { return -1; }
-                if(a.title > b.title) { return 1; }
-                return 0
-            })
-            setSortedMovies(sorted)
-        } else if (selectedSorted === "sortByUserScore") {
-            const sorted = [...movies].sort((a, b) => b.vote_average - a.vote_average)
-            setSortedMovies(sorted)
-        } else if (selectedSorted === 'default') {
-            setSortedMovies([])
-        }
-    }, [selectedSorted, movies])
-
-
+    const {movies, isLoading} = useMovies({categorie})
+    const [sortedMovies, setSelectedSorted] = useSort({items: movies})
     
-    const handlePopularChange = () => setCategorie("popular")
-
-    const handleTopRatedChange = () => setCategorie("top_rated")
-
-    const handleNowPalyingChange = () => setCategorie("now_playing")
-
-    const handleSortChange = (evt) => setSelectedSorted(evt.target.value)
-
+    // obtains what movies to list if sorted or not
     const moviesToList = sortedMovies.length ? sortedMovies : movies
+
+    if (isLoading) {
+        return <ListOfMoviesLoader />
+    }
 
     return (
         <div className="Home">
             <div className="Categories">
-                <Button handleClick={handlePopularChange} text="populares"/>
-                <Button handleClick={handleTopRatedChange} text="mejores valorados"/>
-                <Button handleClick={handleNowPalyingChange} text="mas vistos"/>
+                <MovieCategories setCategorie={setCategorie} />
             </div>
             <div className="Actions">
-                <select onChange={handleSortChange}>
-                    <option value="default">Default</option>
-                    <option value="sortAlphabetically">alfabéticamente</option>
-                    <option value="sortByUserScore">puntuación de usuarios</option>
-                </select>
+                <SelectSort setSelected={setSelectedSorted} />
             </div>
                 
-            <ListOfMovies movies={moviesToList} handleFav={setFavs}/>
+            <ListOfMovies movies={moviesToList} />
         </div>
     )
 }
