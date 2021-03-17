c293596a2130cdb39853fdd17bbeabe5d95f74b1
alexandro <alexandroyedra@gmail.com>
2021-03-17 00:09:17 -0700

    updated ListOfMovies to knows if a movie is fav

--- src/components/ListOfMovies/index.js
+++ src/components/ListOfMovies/index.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import Movie from 'components/Movie'
+import { useSelector } from 'react-redux'
 
 
 import './ListOfMovies.css'
 
 export default function ListOfMovies({movies}) {
+    const favs = useSelector(state => state.favs)
 
     return (
         <div className="ListOfMovies">
             {
-                movies.map(({id, title, poster_path, vote_average}) =>
-                    <Movie
+                movies.map(({id, title, poster_path, vote_average}) => {
+                    const isFav = favs.includes(id)
+                    return <Movie
                         key={id}
                         id={id}
                         title={title}
                         poster={poster_path}
                         usersScore={vote_average}
+                        isFav={isFav}
                     />
-                )
+                })
             }
         </div>
     )
